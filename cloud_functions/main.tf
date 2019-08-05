7a3bca9dadcebd7f11fb3c8c74a47c210c8ee61f
Tom Stockton <tom@stocktons.org.uk>
2019-08-05 17:09:47 +0100

    project_id

--- cloud_functions/main.tf
+++ cloud_functions/main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 module "classify" {
-  source  = "github.com/fuzzylabs/terraform-google-cloud-function?ref=initial_branch"
+  source = "github.com/fuzzylabs/terraform-google-cloud-function?ref=initial_branch"
 
-  project = var.project
-  name    = "classify"
-  runtime = "python37"
+  project_id            = var.project
+  name                  = "classify"
+  runtime               = "python37"
   source_archive_bucket = "${var.billing_org_id}_${var.customer}_${var.project_group}_${var.env}_cloud-functions"
   source_archive_object = "classify.zip"
-  entry_point = "classify"
+  entry_point           = "classify"
 }
